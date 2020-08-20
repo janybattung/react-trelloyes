96b44de1641962c4d91627dae43c90bc22d4be72
Jany Battung <janybattung@gmail.com>
2020-08-20 01:34:13 -0700

    add files

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Card.css';
+
+// function Card(props) { //component shld be capital
+export default function Card(props) {
+    return (
+        <div className='Card'>
+            <button
+                type='button'
+            >
+                delete
+            </button>
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+        </div>
+    )
+}
